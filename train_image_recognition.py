997a9e88fa53c88004896065f04d6daaa5a051f2
Matt <mgreen1313@gmail.com>
2017-05-10 17:32:18 -0400

    feat: Add input layer functions of neural network: image input, label input, keep probability input

--- train_image_recognition.py
+++ train_image_recognition.py
@@ -8,6 +8,30 @@ Created on Wed May 10 16:59:36 2017
 import pickle
 import problem_unittests as tests
 import helper
+import tensorflow as tf
 
 # Load the Preprocessed Validation data
 valid_features, valid_labels = pickle.load(open('preprocess_validation.p', mode='rb'))
+
+# %%
+
+# Build each neural network layer into a function.
+# INPUT LAYER:
+
+def neural_net_image_input(image_shape):
+    return tf.placeholder(tf.float32, shape=(None, image_shape[0], image_shape[1], image_shape[2]), name='x')
+
+
+def neural_net_label_input(n_classes):
+    return tf.placeholder(tf.float32, shape=(None, n_classes), name='y')
+
+
+def neural_net_keep_prob_input():
+    return tf.placeholder(tf.float32, name='keep_prob') 
+
+
+tf.reset_default_graph()
+tests.test_nn_image_inputs(neural_net_image_input)
+tests.test_nn_label_inputs(neural_net_label_input)
+tests.test_nn_keep_prob_inputs(neural_net_keep_prob_input)
+
