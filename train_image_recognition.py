cea4b36ff0f33abfd6a9eacbe1e5bd8c7ebc48fa
Matt <mgreen1313@gmail.com>
2017-05-10 18:29:18 -0400

    feat: Add convolution and max pooling layer function

--- train_image_recognition.py
+++ train_image_recognition.py
@@ -4,6 +4,9 @@ Created on Wed May 10 16:59:36 2017
 
 @author: Matt Green
 """
+import os
+path = "C:/Users/Matt Green/Desktop/version-control/image_recognition"
+os.chdir(path)
 
 import pickle
 import problem_unittests as tests
@@ -35,3 +38,23 @@ tests.test_nn_image_inputs(neural_net_image_input)
 tests.test_nn_label_inputs(neural_net_label_input)
 tests.test_nn_keep_prob_inputs(neural_net_keep_prob_input)
 
+# %%
+
+# CONVOLUTION & MAX POOLING LAYER:
+    
+def conv2d_maxpool(x_tensor, conv_num_outputs, conv_ksize, conv_strides, pool_ksize, pool_strides):
+    weight = tf.Variable(
+                 tf.truncated_normal(
+                     shape=[conv_ksize[0], conv_ksize[1], x_tensor.get_shape().as_list()[3], conv_num_outputs],
+                     stddev=0.1))
+    bias = tf.Variable(tf.zeros(shape=conv_num_outputs))
+    
+    conv = tf.nn.conv2d(x_tensor, weight, strides=[1, conv_strides[0], conv_strides[1], 1], padding='SAME')
+    hidden = tf.nn.relu(conv + bias)
+    sub = tf.nn.max_pool(hidden,
+                         ksize=[1, pool_ksize[0], pool_ksize[1], 1],
+                         strides=[1, pool_strides[0], pool_strides[1], 1],
+                         padding='SAME')
+    return sub
+
+tests.test_con_pool(conv2d_maxpool)
